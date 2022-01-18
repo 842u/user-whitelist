b6a5dad6694ee53de96be38305e671b5970c8dfc
Kamil Bażanow <kamil.bazanow@gmail.com>
2022-01-18 16:11:18 +0100

    Cleaned code and small changes

--- main.py
+++ main.py
@@ -1,49 +1,49 @@
-from textwrap import fill
 import tkinter
 from tkinter import *
 from tkinter import ttk
-from turtle import heading, right
+from tkinter import messagebox
+
+def btn_pressed():
+    messagebox.showinfo(":) Button status message :)", "Button has been pressed.\n  ✌.|•͡˘‿•͡˘|.✌")
 
 root = Tk()
 root.title("842u")
 
-w = 300
-h = 450
-sw = root.winfo_screenwidth()
-sh = root.winfo_screenheight()
-x_coord = int((sw/2) - (w/2))
-y_coord = int((sh/2) - (h/2))
-root.geometry(f"{w}x{h}+{x_coord}+{y_coord}")
-root.resizable(height=False, width=False)
+width = 300
+height = 450
+screen_width = root.winfo_screenwidth()
+screen_heigth = root.winfo_screenheight()
+x_coord = int((screen_width/2) - (width/2))
+y_coord = int((screen_heigth/2) - (height/2))
+root.geometry(f"{width}x{height}+{x_coord}+{y_coord}")
+root.minsize(width,height)
 
 style = ttk.Style()
-# style.configure("style_name.TLabel", foreground = "#00ff44", background = "black")
-style.configure("TLabel", foreground="#ffe600", background="black", font=("",16), justify=CENTER)
-style.configure("TButton", foreground="black", background="black")
+style.configure("N.TFrame", background="black")
+style.configure("N.TLabel", foreground="#ffe600", background="black", font=("",16), justify=CENTER)
+style.configure("WM.TLabel", foreground="#2e2e2e", background="black", font=("",10), justify=CENTER)
+style.configure("N.TButton", foreground="black", background="black")
 
-frm = ttk.Frame(root, style="TLabel")
-# frm.pack(fill=BOTH, expand=False)
-frm.pack(fill=BOTH, expand=True)
+frm_1 = ttk.Frame(root, style="N.TFrame")
+frm_1.pack(fill=BOTH, expand=True)
 
-txt = ttk.Label(frm, text="Hello\n\nEnter master password:")
+txt = ttk.Label(frm_1, text="Hello\n\nEnter master password:", style="N.TLabel")
 txt.pack(anchor=CENTER, pady=10)
 
-ent = ttk.Entry(frm, show="*", justify=CENTER, width=30)
+ent = ttk.Entry(frm_1, show="*", justify=CENTER, width=30)
 ent.pack(pady=20, anchor=CENTER)
 
-btn = ttk.Button(frm, text="CONFIRM", command=root.destroy) ####### CHANGE FROM QUIT TO CONFIRM
-btn.pack(anchor=CENTER)
+btn_confirm = ttk.Button(frm_1, text="CONFIRM", style="N.TButton", command=btn_pressed)
+btn_confirm.pack(anchor=CENTER)
 
 photo = tkinter.PhotoImage(file="img.png")
-# logo = ttk.Label(root, text="asd", image=photo, style="TLabel")
-frm2 = ttk.Frame(frm, style="TLabel")
-frm2.pack(anchor=S, expand=True)
+frm_2 = ttk.Frame(frm_1, style="N.TFrame")
+frm_2.pack(anchor=S, expand=True)
 
-logo = ttk.Label(frm2, image=photo, style="TLabel")
+logo = ttk.Label(frm_2, image=photo, style="N.TLabel")
 logo.pack(fill=BOTH)
 
-wm = ttk.Label(frm, text="842u", justify="right")
-wm.pack(fill=BOTH, side="right")
-
+txt_wm = ttk.Label(frm_1, text="842u", justify="right", style="WM.TLabel")
+txt_wm.pack(fill=BOTH, side="right")
 
 root.mainloop()
