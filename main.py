a2d044989aec50e344091eb5303af5a29e780d3a
Kamil Bażanow <kamil.bazanow@gmail.com>
2022-01-25 15:54:18 +0100

    ACCES WINDOW WITH USER LIST - V.0.1.2

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from turtle import st
 from my_functions import *
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 import tkinter
+
 class MainWindow(tkinter.Tk):   #   TRY ANALIZE AND UNDERSTAND THIS
 
     def __init__(self) -> None:
         tkinter.Tk.__init__(self)
 
-        version = "0.1.1"   #   major.minor.patch
+        version = "0.1.2"   #   major.minor.patch
 
         window_setup(self, title="LOGIN WINDOW", min_width=300, min_height=500, res=False)
 
@@ -55,12 +55,13 @@ class MainWindow(tkinter.Tk):   #   TRY ANALIZE AND UNDERSTAND THIS
 
 
 class NewWindow(tkinter.Toplevel):
-
+    
     def __init__(self, parent) -> None:
         tkinter.Toplevel.__init__(self, parent)
 
-        window_setup(self, title="ACCES WINDOW", min_width=400, min_height=250)
+        window_setup(self, title="ACCES WINDOW", min_width=400, min_height=500)
         style_setup(self, txt_clr="#ffdd00", bg="black", fnt=("Bauhaus 93",16))
+        style_setup(self, style="list.", txt_clr="#5e5e5e", bg="black", fnt=("Bauhaus 93",14))
 
         frm_aw_1 = ttk.Frame(self)
         frm_aw_1.pack(fill=BOTH, expand=True)
@@ -70,19 +71,52 @@ class NewWindow(tkinter.Toplevel):
         frm_aw_1.rowconfigure(1, weight=0)
         frm_aw_1.rowconfigure(tuple(range(2,5)), weight=1)
 
-        lbl_aw_d = ttk.Label(frm_aw_1, text="ADD USER TO WHITELIST").grid(row=1, column=1, columnspan=2, pady=20)
+        lbl_aw_d = ttk.Label(frm_aw_1, text="ADD USER TO WHITELIST")
+        lbl_aw_d.grid(row=1, column=1, columnspan=2, pady=20)
 
-        lbl_aw_n = ttk.Label(frm_aw_1, text="Name: ").grid(row=2, column=1, sticky=W)
-        self.ent_aw_n = ttk.Entry(frm_aw_1).grid(row=2, column=2, sticky=E+W, padx=20)
+        lbl_aw_n = ttk.Label(frm_aw_1, text="Name: ")
+        lbl_aw_n.grid(row=2, column=1, sticky=W)
+        self.ent_aw_n = ttk.Entry(frm_aw_1)     #   https://stackoverflow.com/questions/1101750/tkinter-attributeerror-nonetype-object-has-no-attribute-attribute-name
+        self.ent_aw_n.grid(row=2, column=2, sticky=E+W, padx=20)
 
-        lbl_aw_p = ttk.Label(frm_aw_1, text="Password: ").grid(row=3, column=1, sticky=W)
-        self.ent_aw_p = ttk.Entry(frm_aw_1).grid(row=3, column=2, sticky=E+W, padx=20)
+        lbl_aw_p = ttk.Label(frm_aw_1, text="Password: ")
+        lbl_aw_p.grid(row=3, column=1, sticky=W)
+        self.ent_aw_p = ttk.Entry(frm_aw_1)
+        self.ent_aw_p.grid(row=3, column=2, sticky=E+W, padx=20)
 
-        lbl_aw_e = ttk.Label(frm_aw_1, text="Email: ").grid(row=4, column=1, sticky=W)
-        self.ent_aw_e = ttk.Entry(frm_aw_1).grid(row=4, column=2, sticky=E+W, padx=20)
+        lbl_aw_e = ttk.Label(frm_aw_1, text="Email: ")
+        lbl_aw_e.grid(row=4, column=1, sticky=W)
+        self.ent_aw_e = ttk.Entry(frm_aw_1)
+        self.ent_aw_e.grid(row=4, column=2, sticky=E+W, padx=20)
 
-        btn_aw_au = ttk.Button(frm_aw_1, text="ADD USER").grid(row=5, column=1, pady=20)
-        btn_aw_close = ttk.Button(frm_aw_1, text="CLOSE APP", command=parent.destroy).grid(row=5, column=2, pady=20)
+        btn_aw_au = ttk.Button(frm_aw_1, text="ADD USER", command=self.on_click)
+        btn_aw_au.grid(row=5, column=1, pady=20)
+        btn_aw_cls = ttk.Button(frm_aw_1, text="CLOSE APP", command=parent.destroy)
+        btn_aw_cls.grid(row=5, column=2, pady=20)
+
+        frm_aw_2 = ttk.Frame(self)
+        frm_aw_2.pack(expand=True)
+
+        with open("whitelist.txt", "r") as f:
+            read_data = f.read()
+
+        self.lbl_aw_dr = ttk.Label(frm_aw_2, text=read_data, style="list.TLabel")
+        self.lbl_aw_dr.pack()
+
+    def on_click(self):
+
+        name = self.ent_aw_n.get()  #   MAKE "IF USER EXIST"
+        password = self.ent_aw_p.get()
+        email = self.ent_aw_e.get()
+
+        if name=="" or password=="" or email=="":
+            messagebox.showerror("DATA INCOMPLETE", "PLEASE ENTER ALL DATA")
+        else:
+            with open("whitelist.txt", "a+") as f:    #   MAYBE MAKE FUNCTION
+                f.write(name+" // "+password+" // "+email+"\n")
+            with open("whitelist.txt", "r") as f:
+                data = f.read()
+            self.lbl_aw_dr.config(text=data)
 
 
 window = MainWindow()
--- my_functions.py
+++ my_functions.py
@@ -3,18 +3,14 @@ from tkinter import ttk
 
 def window_setup(window_name, title="new window", min_width=100, min_height=100, res=True):
 
-    #   TITLE
     window_name.title(f"{title}")
 
-    #   SIZE
     window_name.min_width = min_width
     window_name.min_height = min_height
 
-    #   RESIZABLE
     if res == False:
         window_name.resizable(0,0)
 
-    #   CENTER
     screen_width = window_name.winfo_screenwidth()
     screen_height = window_name.winfo_screenheight()
     x_coord = int((screen_width/2) - (min_width/2))
