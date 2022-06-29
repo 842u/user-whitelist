28ebd020aeefbbed47867b8b03933557d78bb1ba
Kamil Bażanow <kamil.bazanow@gmail.com>
2022-06-29 12:10:25 +0200

    unfinished python project

---
+++ functions.py
@@ -0,0 +1,218 @@
+import tkinter
+from tkinter import *
+from tkinter import ttk
+
+def window_setup(window_master, title='new window', min_width=100, min_height=100, res=True):
+    window_master.title(f'{title}')
+
+    window_master.min_width = min_width
+    window_master.min_height = min_height
+
+    if res == False: window_master.resizable(0,0)
+
+    screen_width = window_master.winfo_screenwidth()
+    screen_height = window_master.winfo_screenheight()
+
+    x_pos = int((screen_width/2) - (min_width/2))
+    y_pos = int((screen_height/2) - (min_height/2))
+    
+    window_master.minsize(window_master.min_width,window_master.min_height)
+    window_master.geometry(f'{window_master.min_width}x{window_master.min_height}+{x_pos}+{y_pos}')
+
+def style_setup(self, style='', txt_clr='black', bg='white', fnt=('',14),):
+    self.style = ttk.Style()
+
+    self.style.configure(style+'TLabel', foreground=txt_clr, background=bg, font=fnt)
+
+    widgets = ['TFrame', 'TButton']
+    for i in widgets:
+        self.style.configure(style+i, background=bg)
+
+def update_treeview(treeview, data):
+    treeview.delete(*treeview.get_children())
+
+    for x in range(len(data)):
+        data.loc[x,'index'] = x+1
+        treeview.insert(parent='', index='end', iid=x, text='', 
+            values=(
+                x+1,
+                data.loc[x,'name'],
+                data.loc[x,'password'],
+                data.loc[x,'email']
+            )
+        )
+
+def login_window_layout(self):
+    #   SETUP
+    window_setup(self.login_window_master, 'LOGIN WINDOW', 300, 500)
+    style_setup(self, txt_clr='#ffdd00', bg='black', fnt=('Bauhaus 93',14))
+    style_setup(self, style='foot.', txt_clr='#5e5e5e', bg='black', fnt=('',8))
+
+    #   FRAME
+    self.lw_frm_1 = ttk.Frame(self)
+    self.lw_frm_1.pack(fill=BOTH, expand=True)
+    self.lw_frm_2 = ttk.Frame(self)
+    self.lw_frm_2.pack(fill=BOTH, anchor=S)
+
+    #   IMAGE
+    photo = tkinter.PhotoImage(file='img.png')
+    self.logo = ttk.Label(self.lw_frm_2, image=photo)
+    self.logo.pack(pady=50)
+    self.logo.photo = photo
+
+    #   LABEL
+    self.lw_lbl_top = ttk.Label(self.lw_frm_1, text='HELLO STRANGER\n\nENTER MASTER PASSWORD:\n\nits 123 ;) for testing', justify=CENTER)
+    self.lw_lbl_top.pack(pady=10)
+    self.lw_lbl_ft = ttk.Label(self.lw_frm_2, text='842u', style='foot.TLabel')
+    self.lw_lbl_ft.pack(side='right')
+    self.lw_lbl_vr = ttk.Label(self.lw_frm_2, text=f'{self.version}', style='foot.TLabel',)
+    self.lw_lbl_vr.pack(side='left')
+
+    #   ENTRY
+    self.lw_ent_p = ttk.Entry(self.lw_frm_1, show='*', justify=CENTER)
+    self.lw_ent_p.pack(pady=10)
+
+    #   BUTTON
+    self.lw_btn_li = ttk.Button(self.lw_frm_1, text='LOG IN', command=self.on_click_btn_li)
+    self.lw_btn_li.pack()
+
+def acces_window_layout(self):
+    #   SETUP
+    window_setup(self.acces_window_master, 'ACCES WINDOW', 600, 700)
+
+    #   FRAME
+    self.aw_frm_1 = ttk.Frame(self)
+    self.aw_frm_1.pack(fill=BOTH, expand=True)
+
+    self.aw_frm_a = ttk.Frame(self)
+    self.aw_frm_a.pack(fill=BOTH, expand=True)
+
+    self.aw_frm_b = ttk.Frame(self.aw_frm_a, width=10)
+    self.aw_frm_b.pack(fill=BOTH, side='right')
+
+    self.aw_frm_n = ttk.Frame(self)
+    self.aw_frm_n.pack(fill=BOTH, expand=True)
+
+    self.aw_frm_p = ttk.Frame(self)
+    self.aw_frm_p.pack(fill=BOTH, expand=True)
+
+    self.aw_frm_e = ttk.Frame(self)
+    self.aw_frm_e.pack(fill=BOTH, expand=True)
+
+    self.aw_frm_2 = ttk.Frame(self)
+    self.aw_frm_2.pack(fill=BOTH, expand=True)
+
+    #   LABEL
+    self.aw_lbl_top = ttk.Label(self.aw_frm_1, justify=CENTER, text='WHITELIST')
+    self.aw_lbl_top.pack(pady=10)
+
+    self.aw_lbl_n = ttk.Label(self.aw_frm_n, text='User :', width=10)
+    self.aw_lbl_n.pack(side='left', padx=20)
+
+    self.aw_lbl_p = ttk.Label(self.aw_frm_p, text='Password :', width=10)
+    self.aw_lbl_p.pack(side='left', padx=20)
+
+    self.aw_lbl_e = ttk.Label(self.aw_frm_e, text='Email :', width=10)
+    self.aw_lbl_e.pack(side='left', padx=20)
+
+    #   ENTRY
+    self.aw_ent_n = ttk.Entry(self.aw_frm_n)
+    self.aw_ent_n.pack(side='right', padx=20, expand=True, fill=X)
+
+    self.aw_ent_p = ttk.Entry(self.aw_frm_p)
+    self.aw_ent_p.pack(side='right', padx=20, expand=True, fill=X)
+
+    self.aw_ent_e = ttk.Entry(self.aw_frm_e)
+    self.aw_ent_e.pack(side='right', padx=20, expand=True, fill=X)
+
+    #   BUTTON
+    self.aw_btn_au = ttk.Button(self.aw_frm_2, text='ADD USER', command=self.on_click_btn_au)
+    self.aw_btn_au.pack(padx=20)
+
+    self.aw_btn_eu = ttk.Button(self.aw_frm_b, text='EDIT USER', width=15, command=self.on_click_btn_eu)
+    self.aw_btn_eu.pack(padx=20)
+
+    self.aw_btn_du = ttk.Button(self.aw_frm_b, text='DELETE USER', width=15, command=self.on_click_btn_du)
+    self.aw_btn_du.pack(padx=20, pady=20)
+
+    #   TREEVIEW
+    self.aw_tw_1 = ttk.Treeview(self.aw_frm_a, selectmode='browse')
+    self.aw_tw_1.pack(padx=20, fill=BOTH, expand=True, side='left')
+
+    self.aw_tw_1['columns'] = ('index', 'name', 'password', 'email')
+
+    self.aw_tw_1.column('#0', width=0, stretch=NO)
+    self.aw_tw_1.column('index', anchor=CENTER, width=30, stretch=NO)
+    self.aw_tw_1.column('name', anchor=CENTER, width=80)
+    self.aw_tw_1.column('password', anchor=CENTER, width=80)
+    self.aw_tw_1.column('email', anchor=CENTER, width=80)
+
+    self.aw_tw_1.heading('#0', text='',  anchor=CENTER)
+    self.aw_tw_1.heading('index', text='#', anchor=CENTER)
+    self.aw_tw_1.heading('name', text='NAME', anchor=CENTER)
+    self.aw_tw_1.heading('password', text='PASSWORD', anchor=CENTER)
+    self.aw_tw_1.heading('email', text='EMAIL', anchor=CENTER)
+
+def user_window_layout(self, user):
+    #   SETUP
+    window_setup(self.user_window_master, f'#{user+1} USER WINDOW', 400, 300)
+
+    #   FRAME
+    self.uw_frm_1 = ttk.Frame(self)
+    self.uw_frm_1.pack(fill=BOTH, expand=True)
+    self.uw_frm_1.columnconfigure(1, weight=0)
+    self.uw_frm_1.columnconfigure(2, weight=1)
+    self.uw_frm_1.rowconfigure(1, weight=0)
+    self.uw_frm_1.rowconfigure(tuple(range(2,5)), weight=1)
+
+    self.uw_frm_2 = ttk.Frame(self)
+    self.uw_frm_2.pack(fill=BOTH)
+    self.uw_frm_2.columnconfigure(1, weight=0)
+    self.uw_frm_2.columnconfigure(2, weight=1)
+    self.uw_frm_2.rowconfigure(1, weight=0)
+    self.uw_frm_2.rowconfigure(tuple(range(2,5)), weight=1)
+
+    #   TREEVIEW
+    self.uw_tw_1 = ttk.Treeview(self.uw_frm_1, selectmode='none', height=1)
+    self.uw_tw_1.grid(row=1, column=1, columnspan=4)
+
+    self.uw_tw_1['columns'] = ('index', 'name', 'password', 'email')
+
+    self.uw_tw_1.column('#0', width=0, stretch=NO)
+    self.uw_tw_1.column('index', anchor=CENTER, width=25)
+    self.uw_tw_1.column('name', anchor=CENTER, width=100)
+    self.uw_tw_1.column('password', anchor=CENTER, width=100)
+    self.uw_tw_1.column('email', anchor=CENTER, width=100)
+
+    self.uw_tw_1.heading('#0', text='',  anchor=CENTER)
+    self.uw_tw_1.heading('index', text='#', anchor=CENTER)
+    self.uw_tw_1.heading('name', text='NAME', anchor=CENTER)
+    self.uw_tw_1.heading('password', text='PASSWORD', anchor=CENTER)
+    self.uw_tw_1.heading('email', text='EMAIL', anchor=CENTER)
+
+    #   LABEL
+    self.uw_lbl_n = ttk.Label(self.uw_frm_1, text='User : ')
+    self.uw_lbl_n.grid(row=2, column=1, sticky=W, padx=20)
+
+    self.uw_lbl_p = ttk.Label(self.uw_frm_1, text='Password : ')
+    self.uw_lbl_p.grid(row=3, column=1, sticky=W, padx=20)
+
+    self.uw_lbl_e = ttk.Label(self.uw_frm_1, text='Email : ')
+    self.uw_lbl_e.grid(row=4, column=1, sticky=W, padx=20)
+
+    #   ENTRY
+    self.uw_ent_n = ttk.Entry(self.uw_frm_1)
+    self.uw_ent_n.grid(row=2, column=2, sticky=E+W, padx=20)
+
+    self.uw_ent_p = ttk.Entry(self.uw_frm_1)
+    self.uw_ent_p.grid(row=3, column=2, sticky=E+W, padx=20)
+
+    self.uw_ent_e = ttk.Entry(self.uw_frm_1)
+    self.uw_ent_e.grid(row=4, column=2, sticky=E+W, padx=20)
+
+    #   BUTTON
+    self.uw_btn_s = ttk.Button(self.uw_frm_2, text='SAVE', command=self.on_click_btn_s)
+    self.uw_btn_s.grid(row=1, column=1, padx=20, pady=20)
+
+    self.uw_btn_r = ttk.Button(self.uw_frm_2, text='RESET', command=self.on_click_btn_r)
+    self.uw_btn_r.grid(row=1, column=2, padx=20, pady=20)
--- main.py
+++ main.py
@@ -1,52 +1,169 @@
-from my_functions import *
+import pandas as pd
+from functions import *
 from tkinter import *
-from tkinter import ttk
 from tkinter import messagebox
-import tkinter
-class MakeWindow(tkinter.Tk):   #   TRY ANALIZE AND UNDERSTAND THIS
 
-    def __init__(self) -> None:
-        tkinter.Tk.__init__(self)
+class LoginWindow(Frame):
+    __master_password = '123'
+    version = '0.3.0'
 
-        version = "0.1.0"
+    def __init__(self, master) -> None:
+        Frame.__init__(self, master)
+        self.pack(fill=BOTH, expand=True)
 
-        window_setup(self, title="LOGIN WINDOW", min_width=300, min_height=500, res=True)
+        self.login_window_master = master
 
-        style_setup(self, txt_clr="#ffdd00", bg="black", fnt=("Bauhaus 93",14))     #   style_setup NEED TWEAKS -> MAKE DEFAULT VALUES
-        style_setup(self, style="foot.", txt_clr="#5e5e5e", bg="black", fnt=("",8))
+        login_window_layout(self)
 
-        frm_1 = ttk.Frame(self)
-        frm_1.pack(fill=BOTH, expand=True)
-
-        lbl_1 = ttk.Label(frm_1, text="HELLO STRANGER\n\nENTER MASTER PASSWORD:", justify=CENTER)
-        lbl_1.pack(pady=10)
-
-        self.ent_1 = ttk.Entry(frm_1, show="*", justify=CENTER)
-        self.ent_1.pack(pady=10)
-
-        btn_1 = ttk.Button(frm_1, text="LOG IN", command=self.on_click)
-        btn_1.pack()
-
-        frm_2 = ttk.Frame(self)
-        frm_2.pack(fill=BOTH, anchor=S)
-
-        photo = tkinter.PhotoImage(file="img.png")
-        logo = ttk.Label(frm_2, image=photo)
-        logo.pack(pady=50)
-        logo.photo = photo  #   WITHOUT IT IMG DOESNT DISPLAY -> PhotoImage Garbage Collector PROBLEM 
-
-        lbl_ft = ttk.Label(frm_2, text="842u", style="foot.TLabel")
-        lbl_ft.pack(side="right")
-
-        lbl_vr = ttk.Label(frm_2, text=f"v.{version}", style="foot.TLabel",)
-        lbl_vr.pack(side="left")
-
-    def on_click(self):
-        if  self.ent_1.get() == "asd":
-            messagebox.showinfo("PASSWORD CHECK", "PASSWORD OK")
+    def on_click_btn_li(self):
+        if  self.lw_ent_p.get() == self.__master_password:
+            messagebox.showinfo('PASSWORD OK', 'ACCES GRANTED')
+            self.lw_ent_p.delete(0, 'end')
+            new_toplevel_window = Toplevel(self)
+            new_toplevel_window.iconbitmap('icon.ico')
+            aw = AccesWindow(new_toplevel_window, self)
         else:
-            messagebox.showerror("PASSWORD CHECK", "WRONG PASSWORD")
+            messagebox.showerror('PASSWORD CHECK', 'WRONG PASSWORD')
 
 
-window = MakeWindow()
-window.mainloop()
+class AccesWindow(Frame):
+    __user_limit = 10
+
+    def __init__(self, master, login_window):
+        Frame.__init__(self, master)
+        self.pack(fill=BOTH, expand=True)
+
+        self.acces_window_master = master
+        self.login_window = login_window
+
+        self.data_frame = pd.read_csv('whitelist.csv', index_col=False)
+
+        acces_window_layout(self)
+
+        update_treeview(treeview=self.aw_tw_1, data=self.data_frame)
+        
+    def on_click_btn_au(self):
+        actual_number_of_users = len(self.data_frame)
+
+        new_user_data = [
+            actual_number_of_users+1,
+            self.aw_ent_n.get(),
+            self.aw_ent_p.get(),
+            self.aw_ent_e.get()
+        ]
+
+        if self.aw_ent_n.get()=="" or self.aw_ent_p.get()=="" or self.aw_ent_e.get()=="":
+
+            messagebox.showerror("DATA INCOMPLETE", "PLEASE ENTER ALL DATA", parent=self.acces_window_master)
+
+        elif actual_number_of_users < self.__user_limit:
+            
+            self.aw_ent_n.delete(0, 'end')
+            self.aw_ent_p.delete(0, 'end')
+            self.aw_ent_e.delete(0, 'end')
+            
+            self.data_frame.loc[-1] = new_user_data
+            self.data_frame = self.data_frame.reset_index(drop=True)
+
+            actual_number_of_users = len(self.data_frame)
+
+            self.aw_tw_1.insert(parent='', index='end', iid=actual_number_of_users, text='', 
+                values=(
+                    self.data_frame.loc[actual_number_of_users-1, 'index'],
+                    self.data_frame.loc[actual_number_of_users-1,'name'],
+                    self.data_frame.loc[actual_number_of_users-1,'password'],
+                    self.data_frame.loc[actual_number_of_users-1,'email']
+                )
+            )
+
+            update_treeview(treeview=self.aw_tw_1, data=self.data_frame)
+
+            self.data_frame.to_csv('whitelist.csv', index=False)
+
+        else:
+            messagebox.showerror("USER NUMBER", "MAXIMUM NUMBER OF USERS REACHED", parent=self.acces_window_master)
+
+    def on_click_btn_eu(self):
+        if self.aw_tw_1.focus():
+            self.selected_user = int(self.aw_tw_1.focus())
+
+            new_toplevel_window = Toplevel(self)
+            new_toplevel_window.iconbitmap('icon.ico')
+            uw = UserWindow(new_toplevel_window, self)
+        else:
+            messagebox.showerror("USER NOT SELECTED", "PLEASE SELECT USER TO EDIT", parent=self.acces_window_master)
+
+    def on_click_btn_du(self):
+        if self.aw_tw_1.focus():
+            self.selected_user = int(self.aw_tw_1.focus())
+
+            self.data_frame = self.data_frame.drop(self.selected_user)
+            self.data_frame = self.data_frame.reset_index(drop=True)
+
+            update_treeview(treeview=self.aw_tw_1, data=self.data_frame)
+
+            self.data_frame.to_csv('whitelist.csv', index=False)
+        else:
+            messagebox.showerror("USER NOT SELECTED", "PLEASE SELECT USER TO DELETE", parent=self.acces_window_master)
+
+
+class UserWindow(Frame):
+    def __init__(self, master, acces_window):
+        Frame.__init__(self, master)
+        self.pack(fill=BOTH, expand=True)
+
+        self.user_window_master = master
+        self.acces_window = acces_window
+
+        self.bufored_user_data = self.acces_window.data_frame.loc[self.acces_window.selected_user]
+
+        user_window_layout(self, self.acces_window.selected_user)
+
+        self.uw_ent_n.insert(0, self.bufored_user_data[1])
+        self.uw_ent_p.insert(0, self.bufored_user_data[2])
+        self.uw_ent_e.insert(0, self.bufored_user_data[3])
+
+        self.uw_tw_1.insert(parent='', index='end', iid=self.acces_window.selected_user, text='', 
+                values=(
+                    self.acces_window.data_frame.loc[self.acces_window.selected_user, 'index'],
+                    self.acces_window.data_frame.loc[self.acces_window.selected_user,'name'],
+                    self.acces_window.data_frame.loc[self.acces_window.selected_user,'password'],
+                    self.acces_window.data_frame.loc[self.acces_window.selected_user,'email']
+                )
+            )
+
+    def on_click_btn_s(self):
+        edited_user_data = [
+            self.acces_window.selected_user+1,
+            self.uw_ent_n.get(),
+            self.uw_ent_p.get(),
+            self.uw_ent_e.get()
+        ]
+
+        self.acces_window.data_frame.loc[self.acces_window.selected_user] = edited_user_data
+        self.acces_window.data_frame.to_csv('whitelist.csv', index=False)
+
+        self.uw_tw_1.delete(self.acces_window.selected_user)
+        self.uw_tw_1.insert(parent='', index='end', iid=self.acces_window.selected_user, text='', values=edited_user_data)
+
+        update_treeview(treeview=self.acces_window.aw_tw_1, data=self.acces_window.data_frame)
+
+    def on_click_btn_r(self):
+        self.uw_ent_n.delete(0, 'end')
+        self.uw_ent_n.insert(0, self.bufored_user_data[1])
+
+        self.uw_ent_p.delete(0, 'end')
+        self.uw_ent_p.insert(0, self.bufored_user_data[2])
+
+        self.uw_ent_e.delete(0, 'end')
+        self.uw_ent_e.insert(0, self.bufored_user_data[3])
+
+
+def main():
+    root = Tk()
+    root.iconbitmap('icon.ico')
+    lw = LoginWindow(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
